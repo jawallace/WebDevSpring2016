b2035a5cbe34d7adcb78064c4abf9245b42f25c8
Jeffrey Wallace <jwall@Jeffreys-MacBook.local>
2016-01-19 13:18:08 -0500

    Update server file to serve static content from public.

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var express = require('express');
 
 var app = express();
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+// Express Configuration
+app.use(express.static(__dirname + '/public'));
 
-app.get('/hello', function(req, res){
-  res.send('hello world');
-});
+var IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var PORT = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.listen(port, ipaddress);
+// Kick off Express server
+app.listen(PORT, IP_ADDRESS);
+
