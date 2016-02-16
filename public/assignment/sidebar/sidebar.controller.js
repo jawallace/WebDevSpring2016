22dd942f1af5b5182bb0858873f7f29ca5843224
Jeffrey Wallace <jawallace16@gmail.com>
2016-02-15 22:10:54 -0500

    Add sidebar controller.
    assignment2

---
+++ public/assignment/sidebar/sidebar.controller.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular.module('FormBuilderApp')
+           .controller('SidebarController', SidebarController);
+
+    function SidebarController() {
+        var vm = this;
+
+    }
+
+}());
