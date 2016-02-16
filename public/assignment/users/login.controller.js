a8383221daeab5f50a3782f03457fe965d94e637
Jeffrey Wallace <jawallace16@gmail.com>
2016-02-16 13:19:19 -0500

    Implement login controller.
    assignment2

---
+++ public/assignment/users/login.controller.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    angular.module('FormBuilderApp')
+           .controller('LoginController', LoginController);
+
+    function LoginController(UserService, $rootScope, $state) {
+        var vm = this;
+        
+        vm.user = {}; // ng-model
+        vm.login = login;
+
+        function login(user) {
+            UserService.findByUsernameAndPassword(user.username, user.password, function (foundUser) {
+                if (foundUser) {
+                    $rootScope.user = foundUser;
+                    $state.go('profile');
+                }
+            });
+        }
+    }
+
+}());
