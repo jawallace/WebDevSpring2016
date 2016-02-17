aa1c9214501660b79095a815433803af8cc9b03a
Jeffrey Wallace <jawallace16@gmail.com>
2016-02-17 13:42:32 -0500

    Complete sidebar navigation controls.
    assignment2

--- public/assignment/sidebar/sidebar.controller.js
+++ public/assignment/sidebar/sidebar.controller.js
@@ -4,9 +4,20 @@
     angular.module('FormBuilderApp')
            .controller('SidebarController', SidebarController);
 
-    function SidebarController() {
+    function SidebarController($rootScope) {
         var vm = this;
+        
+        vm.isLoggedIn = false;
+        vm.isAdmin = false;
 
+        $rootScope.$watch(function() {
+            return $rootScope.user;
+        }, updateLoggedInStatus);
+
+        function updateLoggedInStatus(newValue, oldValue) {
+            vm.isLoggedIn = !!newValue;
+            vm.isAdmin = newValue && newValue.role === "admin";
+        }
     }
 
 }());
