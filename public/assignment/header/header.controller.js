f2c9c59e212b39c453f189080503968e94f9f004
Jeffrey Wallace <jawallace16@gmail.com>
2016-02-17 13:31:02 -0500

    Implement header display and navigation.
    assignment2

--- public/assignment/header/header.controller.js
+++ public/assignment/header/header.controller.js
@@ -4,9 +4,30 @@
     angular.module('FormBuilderApp')
            .controller('HeaderController', HeaderController);
 
-    function HeaderController() {
+    function HeaderController($rootScope, $state) {
         var vm = this;
 
+        vm.logout = logout;
+
+        // logged in status
+        vm.isLoggedIn = false;
+        vm.username = "";
+        vm.isAdmin = false;
+
+        $rootScope.$watch(function() {
+            return $rootScope.user;
+        }, updateLoggedInStatus);
+
+        function logout() {
+            $rootScope.user = undefined;
+            $state.go('home'); 
+        }
+
+        function updateLoggedInStatus(newValue, oldValue) {
+            vm.isLoggedIn = !!newValue;
+            vm.username = newValue ? newValue.username : "";
+            vm.isAdmin = newValue && newValue.role === "admin";
+        }
     }
 
 }());
