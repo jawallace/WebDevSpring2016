3a2364274acbdf05ba8960d1da490bafef7da8cf
Jeffrey Wallace <jawallace16@gmail.com>
2016-03-08 15:03:27 -0500

    Restructure assignment directory.
    assignment3

--- public/assignment/client/config.js
+++ public/assignment/client/config.js
@@ -12,33 +12,33 @@
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: 'home/home.view.html'
+                templateUrl: 'views/home/home.view.html'
             })
             .state('register', {
                 url: '/register',
-                templateUrl: 'users/register.view.html',
+                templateUrl: 'views/users/register.view.html',
                 controller: 'RegisterController',
                 controllerAs: 'vm'
             })
             .state('login', {
                 url: '/login',
-                templateUrl: 'users/login.view.html',
+                templateUrl: 'views/users/login.view.html',
                 controller: 'LoginController',
                 controllerAs: 'vm'
             })
             .state('profile', {
                 url: '/profile',
-                templateUrl: 'users/profile.view.html',
+                templateUrl: 'views/users/profile.view.html',
                 controller: 'ProfileController',
                 controllerAs: 'vm'
             })
             .state('admin', {
                 url: '/admin',
-                templateUrl: 'admin/admin.view.html'
+                templateUrl: 'views/admin/admin.view.html'
             })
             .state('forms', {
                 url: '/forms',
-                templateUrl: 'forms/forms.view.html',
+                templateUrl: 'views/forms/forms.view.html',
                 controller: 'FormsController',
                 controllerAs: 'vm'
             });
